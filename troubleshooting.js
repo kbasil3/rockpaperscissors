edbb05147f4e22bfeecd5c63b2da0fbc3af8d7a0
kbasil3 <80859204+kbasil3@users.noreply.github.com>
2021-11-13 10:12:37 -0500

    Add files via upload

---
+++ troubleshooting.js
@@ -0,0 +1,18 @@
+//function troubleshooting() {
+	//const a = 1;
+	//const b = 1;
+
+	//let result;
+
+	// Edit between these lines
+	// =================================
+	//result = "a" + "b";
+
+	// =================================
+
+	//return result;
+}
+
+
+// Do not change this 
+//module.exports = troubleshooting;
